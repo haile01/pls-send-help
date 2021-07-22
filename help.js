34f3d756f1928db9d59ee6770698440d23cecc61
pythagore1123 <ledinhhai1312@gmail.com>
2021-07-22 14:39:40 +0700

    Able to work now, gonna update some effects in the future (when I'm feeling ok

---
+++ help.js
@@ -0,0 +1,59 @@
+const message = document.getElementById('message');
+const achievement = document.getElementById('achievement');
+
+// Pushing messages
+const messages = [
+  `Hey...`,
+  `Heard that you're feeling down lately,`,
+  `Wanna feel better?`,
+  `Sometimes when you feel discouraged,`,
+  `And wondering if you're actually ACHIEVED something in your life`,
+  `Just take a look at what you've made,`,
+  `Wish you all the best <3`
+]
+
+const addMessage = (msg) => {
+  const node = document.createElement('p');
+  node.innerText = msg;
+  node.classList.add('msg');
+  message.appendChild(node);
+}
+
+const addMessages = (ind) => {
+  if (ind === messages.length) {
+    shuffle(achievements);
+    console.log(achievements);
+    addAchievements(0);
+    return;
+  }
+  addMessage(messages[ind]);
+  setTimeout(() => addMessages(ind + 1), 0);
+}
+
+const addAchievement = (acv) => {
+  const node = document.createElement('img');
+  node.src = acv
+  node.classList.add('acv');
+  achievement.appendChild(node);
+}
+
+const addAchievements = (ind) => {
+  if (ind === achievements.length) {
+    return;
+  }
+  addAchievement(achievements[ind]);
+  setTimeout(() => addAchievements(ind + 1), 1000);
+}
+
+const shuffle = (arr) => {
+  n = parseInt(Math.random() * 1000);
+  while (n--) {
+    a = parseInt(Math.random() * (arr.length - 1));
+    b = parseInt(Math.random() * (arr.length - a) + a);
+    let tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+  }
+}
+
+addMessages(0);
--- pls-send-help.py
+++ pls-send-help.py
@@ -3,6 +3,7 @@
 import sys
 import os
 from PIL import ImageGrab
+import subprocess
 
 def is_jpg (content):
   return content[:2] == b'\xff\xd8'
@@ -13,11 +14,12 @@ def is_png (content):
 def invalid_file ():
   print('Oops, it seems like the file is not image :(')
 
-def count_files ():
+def get_files ():
   path = './acv/'
   if not os.path.exists(path):
     os.makedirs(path)
-  return len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
+  files = ["'" + os.path.join(path, f)[2:] + "'" for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+  return files
 
 def add_image (path):
   file_content = open(path, 'rb').read()
@@ -26,7 +28,7 @@ def add_image (path):
     return
   
   file_type = path.split('.')[-1]
-  cnt_file = count_files()
+  cnt_file = len(get_files())
   open('./acv/' + str(cnt_file) + '.' + file_type, 'wb').write(file_content)
   
   print('Yay, one more achievement added :3')
@@ -41,10 +43,21 @@ def add_image_clipboard ():
     add_image(image[0])
     return
 
-  cnt_file = count_files()
+  cnt_file = len(get_files())
   image.save('./acv/' + str(cnt_file) + '.png', 'PNG')
   
   print('Yay, one more achievement added :3')
+
+def send_help ():
+  # Edit template file, too lazy to run server and render_template :(
+  # I'm sad duh
+  template = open('help.html', 'r').read()
+  begin_acv = template.find('// BEGIN ACV') + len('// BEGIN ACV')
+  end_acv = template.find('// END ACV')
+
+  files = ',\n'.join(get_files())
+  template = template[:begin_acv] + '\n' + files + '\n' + template[end_acv:]
+  open('help.html', 'w').write(template)
 
 if len(sys.argv) > 1:
   if sys.argv[1] == '-a':
@@ -53,3 +66,6 @@ if len(sys.argv) > 1:
     add_image(path)
   elif sys.argv[1] == '-ac':
     add_image_clipboard()
+else:
+  send_help()
+  subprocess.Popen('explorer ' + os.path.abspath('./help.html'))
